6a5b885969acdcdd42ac82e0a029904ced93b3e5
EvanBittar <evanking621@gmail.com>
2026-04-02 10:37:11 +0300

    SQL Seed

--- 1_Basics/3b-Assignment_Variables/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ 1_Basics/3b-Assignment_Variables/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01eb55cd452b178d7ce2178c73a9dc5a568c0e28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74f1b7c264a1b1fcb76e767bc0393b3955a4445")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
