fe3b0dbc4a47685f8e43e9ec004f0a2fead6bd1b
EvanBittar <evanking621@gmail.com>
2026-04-10 09:18:40 +0300

    nothing to commit

--- 1_Basics/3-HelloWorld_Variables/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ 1_Basics/3-HelloWorld_Variables/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec092e8fc25d1d1a64c792afa3331cf6f25348b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d253bcd047ec2883e411d64eedf057b184af06")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
